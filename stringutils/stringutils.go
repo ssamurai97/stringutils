52c64799ff737b8f884db39500d7a5a44290a6a0
kuchlong2 <kuchlong@gmail.com>
2021-10-21 23:11:45 -0500

    edited

--- stringutils/stringutils.go
+++ stringutils/stringutils.go
@@ -3,6 +3,10 @@ package stringutils
 import "strings"
 
 
-func Upper( n string){
+func Upper( n string) string {
    return strings.ToUpper(n)
+}
+
+func Lower(s string)string {
+    return strings.ToLower(s)
 }
